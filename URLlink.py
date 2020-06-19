768f9f8f3c2adcea89676c35a0027ab8790a279b
Salumu Ramazani <66482527+SalumuRamazani@users.noreply.github.com>
2020-06-19 12:09:44 +0200

    Add files via upload

---
+++ URLlink.py
@@ -0,0 +1,14 @@
+#extract Links
+
+import requests, bs4, re
+
+def extract_Links (link):
+    
+  res = requests.get(link)
+  Cont_HTML = bs4.BeautifulSoup(res.text)
+  linkRegex = "^" + link
+  for link in Cont_HTML.findAll('a', attrs={'href': re.compile(linkRegex)}):
+    print (link.get("href"))
+
+extract_Links ("http://www.jeuxvideo.com/")
+	
